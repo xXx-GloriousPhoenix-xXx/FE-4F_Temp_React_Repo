9a2de5697144dd35eb18ed229befd384672ce5eb
RomanPR <prikhodjko.roman@gmail.com>
2025-09-24 22:58:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 
 import Header from "./components/UI/Header/Header";
+import Main from "./components/UI/Main/Main";
+import Footer from "./components/UI/Footer/Footer";
 import Logo from "./img/logo.svg";
 
 function App() {
     return (
         <div className="App">
             <Header logoSrc={Logo}/>
+            <Main/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/UI/Main/Main.jsx
@@ -0,0 +1,16 @@
+import cl from "./Main.module.css";
+
+const Main = () => {
+    return (
+        <main className={cl.Wrapper}>
+            {/* <Hero/> */}
+            {/* <AboutUs/> */}
+            {/* <GearShop /> */}
+            {/* <Gallery /> */}
+            {/* <Review /> */}
+            {/* <Blog /> */}
+        </main>
+    );
+}
+
+export default Main;
