07f0c2afa5e948d307c5b366251f06c1c3289c24
RomanPR <prikhodjko.roman@gmail.com>
2025-09-30 12:43:36 +0300

    landing done

--- lab8/src/App.js
+++ lab8/src/App.js
@@ -1,25 +1,18 @@
 import './App.css';
 
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import Landing from './pages/Landing/Landing';
 
 function App() {
-    const navbarProps = [
-        { text: 'Campsites', ref: '' },
-        { text: 'Shop', ref: '' },
-        { text: 'Blog', ref: '' },
-        { text: 'About Us', ref: '' },
-        { text: 'Contact', ref: '' }
-    ];
-    const footerText = "Copyright © 2025 CampOut.com";
-    return (
-        <div className="App">
-            <Header navbarProps={navbarProps}/>
-            <Main/>
-            <Footer text={footerText}/>
-        </div>
-    );
+    return <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing/>} />
+                <Route path="/landing" element={<Landing/>} />
+            </Routes>
+        </BrowserRouter>
+    </div>
 }
 
 export default App;
--- lab8/src/components/AboutUs/AboutUs.jsx
+++ lab8/src/components/AboutUs/AboutUs.jsx
@@ -1,4 +1,5 @@
 import cl from './AboutUs.module.css';
+import textcl from '../../styles/text-hue.module.css';
 
 const AboutUs = () => {
     return (
@@ -12,16 +13,16 @@ const AboutUs = () => {
                         controls
                     ></video>
                 </div>
-                <div className={cl.CommentWrapper}>
-                    <h2>LIVE CAMPING EXPERIENCE</h2>
-                    <h3>We share real camping adventures</h3>
+                <div className={[cl.CommentWrapper, textcl.Dark].join(' ')}>
+                    <div className={cl.Header}>
+                        <h2>LIVE CAMPING EXPERIENCE</h2>
+                        <h3>We share real camping adventures</h3>
+                    </div>
                     <p>
                         Watch our team set up camps, cook outdoors, and explore wild nature.  
                         Experience the spirit of adventure and learn how to prepare for your own trips.
                     </p>
-                    <p>
-                        <em>“Adventure is out there, join us!”</em>
-                    </p>
+                    <em>“Adventure is out there, join us!”</em>
                 </div>
             </div>
         </section>
